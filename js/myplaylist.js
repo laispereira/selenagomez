37b8db1dbfe0f4481c36e75d63c704459db6b194
laispereira <lais_lnp@hotmail.com>
2017-06-02 13:51:08 -0300

    enviando os arquivos pela primeira vez

---
+++ js/myplaylist.js
@@ -0,0 +1,56 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/01-music.mp3',
+        title:'Rumba',
+        artist:'Anahi',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:35',
+        cover:'mix/01.jpg'
+    },
+
+    {
+        mp3:'mix/02-music.mp3',
+        title:'Touch',
+        artist:'Little Mix',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:37',
+        cover:'mix/02.jpg'
+    },
+    {
+        mp3:'mix/03-music.mp3',
+        title:'Kill Em With Kindness',
+        artist:'Selena Gomez',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'3:44',
+        cover:'mix/03.png'
+    },
+
+
+    {
+        mp3:'mix/05-music.mp3',
+        title:'Give me Love ',
+        artist:'Ed Sheeran',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'4:22',
+        cover:'mix/05.jpg'
+    },
+
+
+];
